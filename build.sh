f1ffbd8f7b3a7f3d44f88cef30cd5e24c3adffcd
kadhaDeemi <dami.valderrama@duocuc.cl>
2026-04-15 10:17:31 -0400

    Fix: Optimizando compilación de dlib y agregando opencv

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
-
-pip install -r requirements.txt
+export CMAKE_BUILD_PARALLEL_LEVEL=1
+pip install --no-cache-dir -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
